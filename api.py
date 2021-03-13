844accfb26464099577061505669316e72557746
Arjun Venkatraman <mojoarjun@gmail.com>
2021-03-13 18:45:56 +0530

    Updated

--- api.py
+++ api.py
@@ -1,25 +1,23 @@
 from flask import Flask, request
 from flask import jsonify
-import xetrapal
-import pandas
-from xetrapal import gdastras
 from arango import ArangoClient
-import copy
+import json
+from flask_restful import reqparse, abort, Api, Resource
+from flask_cors import CORS
+confjson = {}
+with open("~/api.json", "rb") as f:
+    confjson = json.loads(f.read())
 
 
-a = xetrapal.karma.load_xpal_smriti("/opt/av-appdata/avxpal.json")
-avxpal = xetrapal.Xetrapal(a)
-config = xetrapal.karma.load_config_json(a.configfile)
-pygsheetsconfig = xetrapal.karma.load_config_json(
-    config['Pygsheets']['avdrive'])
-gd = gdastras.gd_get_googledriver(pygsheetsconfig)
 client = ArangoClient()
-db = client.db("LinkedIn-AV", username="root", password="theansweris42")
-graph = db.graph("linkedinav")
-nodes = graph.vertex_collection("nodes")
-edges = graph.edge_collection("edges")
+db = client.db(confjson['dbname'], username=confjson['username'], password=confjson['password'])
+graph = db.graph(confjson['graphname'])
+
 
 app = Flask(__name__)
+CORS(app)
+api = Api(app)
+
 resp = {
     "resp": None,
     "status": "error"
@@ -31,41 +29,31 @@ def strip_junk(answer):
     return answer
 
 
-@app.route('/hello/', methods=['GET', 'POST'])
-def welcome():
-    return jsonify({
-        "story": "Hello World!",
-        "status": "success"
-    })
-
-
-@app.route('/nodes/', methods=['GET', 'POST'])
-def get_nodes():
-    myresp = copy.deepcopy(resp)
-    if request.method == "GET":
-        if request.args.get("id") is not None:
-            try:
-                answer = strip_junk(dict(nodes.get(request.args.get("id"))))
-                myresp['resp'] = [answer]
-                myresp['status'] = "success"
-            except Exception as e:
-                myresp['resp'] = [str(e)]
-                resp['status'] = "error"
+# Collections CRUD
+class Collections(Resource):
+    def get(self, collection_name=None):
+        if collection_name is not None:
+            count = graph.vertex_collection(collection_name).count()
+            return jsonify({
+                "resp": [{
+                    "count": count,
+                    "collection_name": collection_name
+                }],
+                "status": "success"
+            })
         else:
-            myresp['resp'] = [nodes.all().count()]
-            myresp['status'] = "success"
-    if request.method == "POST":
-        try:
-            nodes.update(request.json)
-            p = strip_junk(nodes.get(request.json['_key']))
-            myresp['resp'] = [p]
-            myresp['status'] = "success"
-        except Exception as e:
-            myresp['resp'] = [str(e)]
-            resp['status'] = "error"
+            return jsonify({
+                "resp": [{
+                    "count": graph.vertex_collection(x).count(),
+                    "collection_name": x
+                } for x in graph.vertex_collections()],
+                "status": "success"
+            })
 
-    return jsonify(myresp)
 
+
+
+api.add_resource(Collections, '/collections/<collection_name>', '/collections', '/collections/')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5010)
